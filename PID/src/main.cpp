ff8a4df5021c0580e80c5baedef890af2b45d22f
Angela Qian <139920088+Angie-Qian@users.noreply.github.com>
2025-09-05 14:10:47 -0400

    Update main.cpp

--- PID/src/main.cpp
+++ PID/src/main.cpp
@@ -13,6 +13,13 @@ using namespace vex;
 
 // A global instance of competition
 competition Competition;
+brain Brain;
+
+inertial spin = inertial (PORT1);
+
+//motors
+motor lMotor = motor(PORT2);
+motor rMotor = motor(PORT3);
 
 // define your global instances of motors and other devices here
 
@@ -27,7 +34,8 @@ competition Competition;
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-
+//callirating
+spin.calibrate();
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
@@ -41,11 +49,101 @@ void pre_auton(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
+float turnTolerance=1; //tolerance is 1 degree
+
+void turnNoPID(float target){ //target: set turning amount , global on global frame
+
+  while(1){
+
+float currentHeading = spin.heading(); //set the current heading
+float rTurn = target-currentHeading;
+float lTurn = currentHeading-target;
+//correct turns if negative
+if (lTurn<0){
+  lTurn+=360;
+}
+if (rTurn<0){
+  rTurn+=360;
+}
+//how much degrees you need to turn locally
+float turnDistance = 0;
+//find most efficent path
+if (rTurn<lTurn){
+  turnDistance=rTurn;
+}
+else{
+  turnDistance=-lTurn;
+}
+//check if we are close enough to stop (tolerance
+if(currentHeading<=target+turnTolerance and currentHeading >= target-turnTolerance){
+  rMotor.stop();
+  lMotor.stop();
+  break; //out of while loop
+}
+//spin motors
+rMotor.spin(reverse, 20, pct); //lower speed
+lMotor.spin(fwd,20,pct);
+
+wait(20,msec);
+  }
+
+}
+
+
+//k: turning values
+float kP=0; //potential, speed up if there is a longer distance, slow down if shorter
+float kI=0; //intergral, not used in turning (keep in 0), faster based on distance travelled
+float kD=0; //Derivative, smooth stop, based on speed, faster if high speed
+
+void turnPID(float target){ //target: set turning amount , global on global frame
+ float turnIntegral =0; //PID, total distance travelled
+ float prevTurnDist=0;
+  while(1){
+
+float currentHeading = spin.heading(); //set the current heading
+float rTurn = target-currentHeading;
+float lTurn = currentHeading-target;
+//correct turns if negative
+if (lTurn<0){
+  lTurn+=360;
+}
+if (rTurn<0){
+  rTurn+=360;
+}
+//how much degrees you need to turn locally
+float turnDistance = 0;
+//find most efficent path
+if (rTurn<lTurn){
+  turnDistance=rTurn;
+}
+else{
+  turnDistance=-lTurn;
+}
+//check if we are close enough to stop (tolerance
+if(currentHeading<=target+turnTolerance and currentHeading >= target-turnTolerance){
+  rMotor.stop();
+  lMotor.stop();
+  break; //out of while loop
+}
+
+//PID calculations:
+float turnDerivative=turnDistance-prevTurnDist;
+float turnSpeed=(kP*turnDistance)+(kI)+(kD*turnDerivative); 
+//spin motors
+rMotor.spin(reverse, turnSpeed, volt); 
+lMotor.spin(fwd,turnSpeed,volt); //use volt bc reduce strain on vex brain
+//update the prev dist
+prevTurnDist=turnDistance;
+wait(10,msec);
+  }
+
+}
+
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+waitUntil(spin.isCalibrating()==false); // wait for it to stop calibrating
+spin.setHeading(0,deg);
+turnNoPID(10);
 }
 
 /*---------------------------------------------------------------------------*/
